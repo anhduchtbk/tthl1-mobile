bdae6bc0565c17dc71804f5067026cc66a816ffb
PHAM DUC ANH <anhduchtbk@gmail.com>
2025-12-17 21:09:07 +0700

    update ui

--- src/app/(tabs)/military-number/military-history-report.tsx
+++ src/app/(tabs)/military-number/military-history-report.tsx
@@ -1,106 +1,10 @@
 import FilterButton from '@/components/common/Button/filter-button';
 import { Box } from '@/components/common/Layout/Box';
-import { Text } from '@/components/common/Text/Text';
 import { ScreenHeader } from '@/components/header/ScreenHeader';
 import { DayElementScrollView } from '@/features/home/timetable';
+import { HistoryElements } from '@/features/manage-student/history/HistoryElements';
 import { colors } from '@/theme/colors';
-import { LinearGradient } from 'expo-linear-gradient';
 import { ScrollView, StyleSheet } from 'react-native';
-
-type PropsHistoryElements = {
-  schedule: string; // tiêu đề buổi điểm danh
-  bgColor?: string;
-  borderColor?: string;
-  totalStudents?: number;
-  absentStudents?: number;
-  actualStudents?: number;
-  listStudentGroup?: PropsStudentGroup[];
-};
-
-type PropsStudentGroup = {
-  numberGroup?: number;
-  reseasonAbsent?: string;
-  absentStudents?: string[];
-};
-
-function StudentGroup({
-  numberGroup,
-  reseasonAbsent,
-  absentStudents,
-}: PropsStudentGroup) {
-  return (
-    <Box
-      borderWidth={1}
-      borderColor={colors.primary[10]}
-      borderRadius={10}
-      borderStyle="dashed"
-      py={4}
-      px={8}
-      gap={4}
-    >
-      <Text fontWeight="bold" fontSize={14} color={colors.primary[10]}>
-        Nhóm học viên {numberGroup}
-      </Text>
-      <Text fontWeight="bold" fontSize={14}>
-        Lý do: {reseasonAbsent}
-      </Text>
-      <Text fontWeight="bold" fontSize={14}>
-        Học viên vắng: {absentStudents?.join(', ')}
-      </Text>
-    </Box>
-  );
-}
-
-function HistoryElements({
-  schedule,
-  bgColor,
-  borderColor,
-  totalStudents,
-  absentStudents,
-  actualStudents,
-  listStudentGroup,
-}: PropsHistoryElements) {
-  return (
-    <Box>
-      <Box
-        backgroundColor={bgColor}
-        flexDirection="row"
-        alignItems="center"
-        gap={12}
-      >
-        <Box
-          borderWidth={3}
-          borderColor={borderColor}
-          borderRadius={3}
-          style={{ minHeight: 20 }}
-          backgroundColor={colors.black}
-        />
-        <Text style={styles.txtSchedule} fontSize={14} color={'#777777'}>
-          {schedule}
-        </Text>
-      </Box>
-      <Box py={8} px={4} gap={4}>
-        <Text fontWeight="bold" fontSize={14}>
-          Tổng quân số: {totalStudents}
-        </Text>
-        <Text fontWeight="bold" fontSize={14}>
-          Vắng: {absentStudents}
-        </Text>
-        <Text fontWeight="bold" fontSize={14}>
-          Quân số thực tế: {actualStudents}
-        </Text>
-        {listStudentGroup?.map((group, index) => (
-          <StudentGroup
-            key={index}
-            numberGroup={index + 1}
-            reseasonAbsent={group.reseasonAbsent}
-            absentStudents={group.absentStudents}
-          />
-        ))}
-      </Box>
-    </Box>
-  );
-}
 
 export default function MilitaryNumberScreen() {
   const dictColors = {
@@ -144,7 +48,6 @@ export default function MilitaryNumberScreen() {
             actualStudents={125}
             listStudentGroup={tempData1}
           />
-
           <HistoryElements
             bgColor={dictColors[2].bgColor}
             borderColor={dictColors[2].borderColor}
@@ -162,15 +65,8 @@ export default function MilitaryNumberScreen() {
           />
         </Box>
       </ScrollView>
-      
     </Box>
   );
 }
 
-const styles = StyleSheet.create({
-  txtSchedule: {
-    fontWeight: 800,
-    lineHeight: 20,
-  },
-  
-});
+const styles = StyleSheet.create({});
